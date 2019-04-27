0bbd806b867010add930cd95ae7a59c35b5d87bc
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-27 23:19:13 +0300

    Modify in index.js

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ var OSinfo = require('./modules/OSInfo');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if(input !== null) {
+    //if(input !== null) {
         var instruction = input.trim();
         switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
+          case '':
+            process.stdout.write('You did not write anything!\n');
+            process.exit();
+            break;
+          case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+          case '/sayhello':
+            process.stdout.write('hello!\n');
+            break;
+          case '/getOSinfo':
+            OSinfo.print();
+            break;
+          default:
+            process.stderr.write('Wrong instruction!\n');
         };
-    }
+    //}
 });
