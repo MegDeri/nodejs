8a9f12727375c12297f276435f92e18ef2653237
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-05-07 21:46:59 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
       var instruction = input.trim();
-      switch(instruction) {
-        case '':
-          process.stdout.write('You did not write anything!\n');
-          process.exit();
-          break;
-        case '/exit':
-          process.stdout.write('Quitting app!\n');
-          process.exit();
-          break;
-        case '/sayhello':
-          process.stdout.write('hello!\n');
-          break;
-        case '/getOSinfo':
-          OSinfo.print();
-          break;
-        default:
-          process.stderr.write('Wrong instruction!\n');
-      };
     }
+    switch(instruction) {
+      case '':
+        process.stdout.write('You did not write anything!\n');
+        process.exit();
+        break;
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case '/sayhello':
+        process.stdout.write('hello!\n');
+        break;
+      case '/getOSinfo':
+        OSinfo.print();
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    };
 });
