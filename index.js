019aeea54588ff72b423df8087896c4ea072143e
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-03-06 20:54:18 +0200

    Modify index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-var os = require('os');
-var OSinfo = require('./modules/OSInfo');
+var os = require("os");
+var OSinfo = require("./modules/OSInfo");
 
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if (input !== null) {
-      var instruction = input.trim();
-      return;
-    }
-    switch(instruction) {
-      case '':
-        process.stdout.write('You did not write anything!\n');
-        process.exit();
-        break;
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
-      case '/sayhello':
-        process.stdout.write('hello!\n');
-        break;
-      case '/getOSinfo':
-        OSinfo.print();
-        break;
-      default:
-        process.stderr.write('Wrong instruction!\n');
-    };
+process.stdin.setEncoding("utf-8");
+process.stdin.on("readable", function() {
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.trim();
+  }
+  switch (instruction) {
+    case "":
+      process.stdout.write("You did not write anything!\n");
+      process.exit();
+      break;
+    case "/exit":
+      process.stdout.write("Quitting app!\n");
+      process.exit();
+      break;
+    case "/sayhello":
+      process.stdout.write("hello!\n");
+      break;
+    case "/getOSinfo":
+      OSinfo.print();
+      break;
+    default:
+      process.stderr.write("Wrong instruction!\n");
+  }
 });
+// console.log("Hello world!");
+// console.log(process.versions);
